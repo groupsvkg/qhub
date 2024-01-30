a68c06189bf3c1e391caf27c8bce31e161a870c6
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-01-30 20:45:38 +0000

    implement post page

--- app/(platform)/(dashboard)/post/page.tsx
+++ app/(platform)/(dashboard)/post/page.tsx
@@ -1,11 +1,39 @@
 import { Sidebar } from "@/components/sidebar";
 
+import { Editor } from "@/components/editor"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const PostPage = () => {
     return (
         <div className="pt-16">
             <Sidebar />
             <div className="md:pl-60 h-full ">
-                Post Page
+                <div className="flex flex-col items-center justify-center p-4">
+                    <Card className="w-full max-w-3xl">
+                        <CardHeader>
+                            <CardTitle className="text-2xl">Post a Problem</CardTitle>
+                            <CardDescription>Post a problem related to any topic</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <form action="" className="space-y-6">
+                                <div className="space-y-2">
+                                    <Label htmlFor="description">Question Description</Label>
+                                    <Editor />
+                                </div>
+                                <div className="space-y-2">
+                                    <Label htmlFor="answer">Answer</Label>
+                                    <Input id="answer" placeholder="Enter your answer or answers, separated by commas."/>
+                                </div>
+                                <Button className="w-full" type="submit">
+                                    Post Problem
+                                </Button>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     );
---
+++ components/editor.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from 'react';
+import 'katex/dist/katex.css';
+import katex from 'katex';
+import { getCodeString } from 'rehype-rewrite';
+import MDEditor from "@uiw/react-md-editor";
+
+const transformLatex = ({ inline, children = [], className, ...props }) => {
+    const txt = children[0] || '';
+    if (inline) {
+      if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
+        const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, '$1'), {
+          throwOnError: false,
+        });
+        return <code dangerouslySetInnerHTML={{ __html: html }} />;
+      }
+      return <code>{txt}</code>;
+    }
+    const code = props.node && props.node.children ? getCodeString(props.node.children) : txt;
+    if (
+      typeof code === 'string' &&
+      typeof className === 'string' &&
+      /^language-katex/.test(className.toLocaleLowerCase())
+    ) {
+      const html = katex.renderToString(code, {
+        throwOnError: false,
+      });
+      return <code style={{ fontSize: '150%' }} dangerouslySetInnerHTML={{ __html: html }} />;
+    }
+    return <code className={String(className)}>{txt}</code>;
+  }
+
+export const Editor = () => {
+    const [value, setValue] = useState("");
+    
+    return (
+        <div className="container">
+            <div className='md:hidden'>
+                <MDEditor
+                    id="description"
+                    value={value}
+                    onChange={setValue}
+                    textareaProps={{
+                        placeholder: 'Please enter Markdown text',
+                    }}
+                    preview='edit'
+                />
+                <MDEditor 
+                    value={value} 
+                    style={{ whiteSpace: 'pre-wrap' }}
+                    preview='preview'
+                    hideToolbar
+                    textareaProps={{
+                        placeholder: 'Please enter Markdown text',
+                    }}
+                    previewOptions={{
+                        components: {
+                            code: {transformLatex},
+                        },
+                    }}
+                />
+            </div>
+            <div className='hidden md:block'>
+                <MDEditor
+                    id="description"
+                    value={value}
+                    onChange={setValue}
+                    textareaProps={{
+                        placeholder: 'Please enter Markdown text',
+                    }}
+                    preview='live'
+                />
+            </div>
+        </div>
+    );
+};
