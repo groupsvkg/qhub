578a18478208e91858a77eb8549efc598e563170
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-01 17:35:10 +0000

    fix padding

--- components/problem/index.tsx
+++ components/problem/index.tsx
@@ -1,6 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { getProblem } from "@/lib/problem-service";
-import MDEditor from "@uiw/react-md-editor";
 import ProblemPreview from "./problem-preview";
 
 interface ProblemProps {
@@ -13,7 +12,7 @@ export const Problem = async ({problemId}: ProblemProps) => {
     return (
         <Card className="w-full md:max-w-4xl">
             <CardHeader>
-                <CardTitle className="text-2xl text-gray-500">
+                <CardTitle className="hidden md:block text-2xl text-gray-500">
                     Created by <span className="">{problem.user.firstName}</span>
                 </CardTitle>
                 <CardDescription>
