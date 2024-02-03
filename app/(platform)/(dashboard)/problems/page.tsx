dd839603a51141df50f1b6d1f4c2ee6985b6f01c
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-03 13:17:47 +0000

    add likes and dislikes count

--- app/(platform)/(dashboard)/problems/page.tsx
+++ app/(platform)/(dashboard)/problems/page.tsx
@@ -1,6 +1,7 @@
 import { Sidebar } from "@/components/sidebar";
+import { Separator } from "@/components/ui/separator";
 import { db } from "@/lib/db";
-import { cn } from "@/lib/utils";
+import { ThumbsDown, ThumbsUp } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -20,12 +21,12 @@ const ProblemsPage = async () => {
                     <div className="w-full mb-4">
                         <h1 className="text-2xl md:text-3xl font-bold">Problems</h1>
                     </div>
-                    <div className="grid gap-2 grid-cols-2 md:grid-cols-4 lg:gricol6">
+                    <div className="grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:gricol6">
                     {
                         problems.map(problem => (
                             <Link key={problem.id} href={`/problems/${problem.id}`} className="hover:bg-gray-200 rounded-md">
                                 <div className="p-2 space-y-2 shadow-md border rounded-md">
-                                    <div className="flex flex-col md:flex-row items-center justify-start">
+                                    <div className="flex items-center justify-start">
                                         <Image 
                                                 src={problem.user.imageUrl}
                                                 height={50}
@@ -36,6 +37,14 @@ const ProblemsPage = async () => {
                                         <div className="flex flex-col ml-2 text-ellipsis overflow-hidden">
                                             <h1 className="text-gray-700 text-md font-semibold">{problem.category}</h1>
                                             <p className="text-gray-500 text-sm">{problem.title}</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center justify-start text-xs text-gray-500 gap-x-1 pt-1">
+                                        <div>
+                                            {problem.likes} likes
+                                        </div>
+                                        <div>
+                                            {problem.dislikes} dislikes
                                         </div>
                                     </div>
                                 </div>
