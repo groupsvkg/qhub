ef7ed5704d65dccb63df5ebe2a85c4d9990c6d3d
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-04 21:11:44 +0000

    add new youtube vedio in help page

--- app/(platform)/(dashboard)/help/page.tsx
+++ app/(platform)/(dashboard)/help/page.tsx
@@ -7,6 +7,7 @@ const HelpPage = () => {
             <Sidebar />
             <div className="md:pl-60 h-full m-2">
                 <div className="space-y-2">
+                    <YouTubeEmbed videoid="0lgv6bn7h2w" params="controls=1" />
                     <YouTubeEmbed videoid="gvTm4dqCxqc" params="controls=1" />
                     <YouTubeEmbed videoid="Cv9u-xDz7I4" params="controls=1" />
                     <YouTubeEmbed videoid="m6P1PZSI8bo" params="controls=1" />
