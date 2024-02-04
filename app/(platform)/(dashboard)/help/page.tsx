7cb378f7b49dd66f81f34e28b465a6de80ffc6d9
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-04 20:56:33 +0000

    improve help page ui

--- app/(platform)/(dashboard)/help/page.tsx
+++ app/(platform)/(dashboard)/help/page.tsx
@@ -5,13 +5,23 @@ const HelpPage = () => {
     return (
         <div className="pt-16">
             <Sidebar />
-            <div className="md:pl-60 h-full space-y-4 m-2">
-                <YouTubeEmbed videoid="gvTm4dqCxqc" height={400} params="controls=1" />
-                <YouTubeEmbed videoid="Cv9u-xDz7I4" height={400} params="controls=1" />
-                <YouTubeEmbed videoid="m6P1PZSI8bo" height={400} params="controls=1" />
-                <YouTubeEmbed videoid="wuoxDy-3SxQ" height={400} params="controls=1" />
-                <YouTubeEmbed videoid="CP8CrZYOo0o" height={400} params="controls=1" />
-                <YouTubeEmbed videoid="dtkoPT20jss" height={400} params="controls=1" />
+            <div className="md:pl-60 h-full m-2">
+                <div className="hidden md:block space-y-4">
+                    <YouTubeEmbed videoid="gvTm4dqCxqc" height={400} params="controls=0" />
+                    <YouTubeEmbed videoid="Cv9u-xDz7I4" height={400} params="controls=0" />
+                    <YouTubeEmbed videoid="m6P1PZSI8bo" height={400} params="controls=0" />
+                    <YouTubeEmbed videoid="wuoxDy-3SxQ" height={400} params="controls=0" />
+                    <YouTubeEmbed videoid="CP8CrZYOo0o" height={400} params="controls=0" />
+                    <YouTubeEmbed videoid="dtkoPT20jss" height={400} params="controls=0" />
+                </div>
+                <div className="md:hidden space-y-4">
+                    <YouTubeEmbed videoid="gvTm4dqCxqc" height={200} params="controls=0" />
+                    <YouTubeEmbed videoid="Cv9u-xDz7I4" height={200} params="controls=0" />
+                    <YouTubeEmbed videoid="m6P1PZSI8bo" height={200} params="controls=0" />
+                    <YouTubeEmbed videoid="wuoxDy-3SxQ" height={200} params="controls=0" />
+                    <YouTubeEmbed videoid="CP8CrZYOo0o" height={200} params="controls=0" />
+                    <YouTubeEmbed videoid="dtkoPT20jss" height={200} params="controls=0" />
+                </div>
             </div>
         </div>
     );
