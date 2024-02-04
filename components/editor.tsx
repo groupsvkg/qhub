1db46b80904c2b883a55e83ed1c447e4a95ed968
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-04 23:25:19 +0000

    fix latex and code rendering issue while preview

--- components/editor.tsx
+++ components/editor.tsx
@@ -6,8 +6,10 @@ import katex from 'katex';
 import { getCodeString } from 'rehype-rewrite';
 import MDEditor from "@uiw/react-md-editor";
 
+
 // @ts-ignore
 const transformLatex = ({ inline, children = [], className, ...props }) => {
+
   const txt = children[0] || '';
   if (inline) {
     if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
@@ -15,11 +17,15 @@ const transformLatex = ({ inline, children = [], className, ...props }) => {
       const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, '$1'), {
         throwOnError: false,
       });
+
       return <code dangerouslySetInnerHTML={{ __html: html }} />;
     }
+
     return <code>{txt}</code>;
   }
+
   const code = props.node && props.node.children ? getCodeString(props.node.children) : txt;
+
   if (
     typeof code === 'string' &&
     typeof className === 'string' &&
@@ -28,9 +34,11 @@ const transformLatex = ({ inline, children = [], className, ...props }) => {
     const html = katex.renderToString(code, {
       throwOnError: false,
     });
+
     return <code style={{ fontSize: '150%' }} dangerouslySetInnerHTML={{ __html: html }} />;
   }
-  return <code className={String(className)}>{txt}</code>;
+
+  return children;
 }
 
 export const Editor = () => {
@@ -74,7 +82,7 @@ export const Editor = () => {
             components: {
               // @ts-ignore
               code: transformLatex,
-            },
+            }
           }}
         />
       </div>
--- components/problem/problem-preview.tsx
+++ components/problem/problem-preview.tsx
@@ -8,17 +8,22 @@ import MDEditor, { code } from "@uiw/react-md-editor";
 // @ts-ignore
 const transformLatex = ({ inline, children = [], className, ...props }) => {
   const txt = children[0] || '';
+
   if (inline) {
     if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
       // @ts-ignore
       const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, '$1'), {
         throwOnError: false,
       });
+
       return <code dangerouslySetInnerHTML={{ __html: html }} />;
     }
+
     return <code>{txt}</code>;
   }
+
   const code = props.node && props.node.children ? getCodeString(props.node.children) : txt;
+
   if (
     typeof code === 'string' &&
     typeof className === 'string' &&
@@ -27,9 +32,11 @@ const transformLatex = ({ inline, children = [], className, ...props }) => {
     const html = katex.renderToString(code, {
       throwOnError: false,
     });
+
     return <code style={{ fontSize: '150%' }} dangerouslySetInnerHTML={{ __html: html }} />;
   }
-  return <code className={String(className)}>{txt}</code>;
+
+  return children;
 }
 
 export const ProblemPreview = ({ problem }: { problem: any }) => {
