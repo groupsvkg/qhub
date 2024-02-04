2b5d1798495081f6cc25d5a9195f47cac65efff8
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-04 15:01:24 +0000

    fix ui layout issue for activity page

--- app/(platform)/(dashboard)/activity/page.tsx
+++ app/(platform)/(dashboard)/activity/page.tsx
@@ -7,7 +7,7 @@ import { getSelf } from "@/lib/auth-service";
 
 const ActivityPage = async () => {
     const user = await getSelf();
-    
+
     const activities = await db.activity.findMany({
         take: 50,
         where: {
@@ -29,15 +29,15 @@ const ActivityPage = async () => {
     return (
         <div className="pt-16">
             <Sidebar />
-            <div className="md:pl-60 h-full ">
+            <div className="md:pl-60 h-full">
                 <div className="h-full flex flex-col m-6 items-center justify-start">
                     <div className="w-full mb-4">
                         <h1 className="text-2xl md:text-3xl font-bold">Activity</h1>
                     </div>
-                    <div className="grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-6">
+                    <div className="grid gap-2 grid-cols-1 md:grid-cols-3 xl:grid-cols-6 w-full">
                     {
                         activities.map(activity => (
-                            <Link key={user.id} href={`/problems/${activity.problemId}`} className="hover:bg-gray-200 rounded-md">
+                            <Link key={activity.id} href={`/problems/${activity.problemId}`} className="hover:bg-gray-200 rounded-md">
                                 <div className="flex flex-col justify-between p-2 space-y-2 shadow-md border rounded-md h-full">
                                     <div className="flex items-center justify-start">
                                         <Image 
