a3f7b440e6181032c309192452e1b3ac776c8eac
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-04 21:09:09 +0000

    fix padding issue in help page

--- app/(platform)/(dashboard)/help/page.tsx
+++ app/(platform)/(dashboard)/help/page.tsx
@@ -6,21 +6,13 @@ const HelpPage = () => {
         <div className="pt-16">
             <Sidebar />
             <div className="md:pl-60 h-full m-2">
-                <div className="hidden md:block space-y-4">
-                    <YouTubeEmbed videoid="gvTm4dqCxqc" height={400} params="controls=0" />
-                    <YouTubeEmbed videoid="Cv9u-xDz7I4" height={400} params="controls=0" />
-                    <YouTubeEmbed videoid="m6P1PZSI8bo" height={400} params="controls=0" />
-                    <YouTubeEmbed videoid="wuoxDy-3SxQ" height={400} params="controls=0" />
-                    <YouTubeEmbed videoid="CP8CrZYOo0o" height={400} params="controls=0" />
-                    <YouTubeEmbed videoid="dtkoPT20jss" height={400} params="controls=0" />
-                </div>
-                <div className="md:hidden space-y-4">
-                    <YouTubeEmbed videoid="gvTm4dqCxqc" height={200} params="controls=0" />
-                    <YouTubeEmbed videoid="Cv9u-xDz7I4" height={200} params="controls=0" />
-                    <YouTubeEmbed videoid="m6P1PZSI8bo" height={200} params="controls=0" />
-                    <YouTubeEmbed videoid="wuoxDy-3SxQ" height={200} params="controls=0" />
-                    <YouTubeEmbed videoid="CP8CrZYOo0o" height={200} params="controls=0" />
-                    <YouTubeEmbed videoid="dtkoPT20jss" height={200} params="controls=0" />
+                <div className="space-y-2">
+                    <YouTubeEmbed videoid="gvTm4dqCxqc" params="controls=1" />
+                    <YouTubeEmbed videoid="Cv9u-xDz7I4" params="controls=1" />
+                    <YouTubeEmbed videoid="m6P1PZSI8bo" params="controls=1" />
+                    <YouTubeEmbed videoid="wuoxDy-3SxQ" params="controls=1" />
+                    <YouTubeEmbed videoid="CP8CrZYOo0o" params="controls=1" />
+                    <YouTubeEmbed videoid="dtkoPT20jss" params="controls=1" />
                 </div>
             </div>
         </div>
