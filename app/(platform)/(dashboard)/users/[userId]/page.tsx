19ab4fb61c1c6c904a871d782958447388d6f643
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-05 14:52:12 +0000

    implement dummy user page

---
+++ app/(platform)/(dashboard)/users/[userId]/page.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "@/components/sidebar";
+
+const UserPage = ({ params }: { params: { userId: string } }) => {
+    return (
+        <div className="pt-16 h-full">
+            <Sidebar />
+            <div className="md:pl-60 h-full">
+                <div className="h-full flex flex-col items-center justify-start p-4">
+                    User Page - {params.userId}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserPage;
