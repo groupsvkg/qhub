7f974cb84a140017244552b325df170edf951807
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-07 12:37:55 +0000

    add icons for post and leadership to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,9 @@
 import { Logo } from "@/components/logo"
 import { UserButton } from "@clerk/nextjs"
-import { Menu } from "lucide-react"
+import { PlusCircle, Trophy } from "lucide-react"
 import { MobileSidebar } from "@/components/mobile-sidebar"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
 
 export const Navbar = () => {
     return (
@@ -10,7 +12,21 @@ export const Navbar = () => {
                 <MobileSidebar />
                 <Logo />
             </div>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-between gap-3">
+                <Link href="/leaderboard" className="hover:bg-gray-200 rounded-full">
+                    <div className={cn(
+                        "flex items-center justify-start",
+                    )}>
+                        <div><Trophy className="h-5 w-5 text-gray-400" /></div>
+                    </div>
+                </Link>
+                <Link href="/post" className="hover:bg-gray-200 rounded-full">
+                    <div className={cn(
+                        "flex items-center justify-start",
+                    )}>
+                        <div><PlusCircle className="h-5 w-5 text-gray-400" /></div>
+                    </div>
+                </Link>
                 <UserButton afterSignOutUrl="/" />
             </div>
         </nav>
