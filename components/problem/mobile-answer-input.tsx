fa528b26859b3003c291eb423947753af60a21fc
Vishal Kumar Gupta <groupsvkg@gmail.com>
2024-02-08 17:02:45 +0000

    test after cleanup

--- components/problem/mobile-answer-input.tsx
+++ components/problem/mobile-answer-input.tsx
@@ -22,53 +22,51 @@ export const MobileAnswerInput = ({ problemId }: MobileAnswerInputProps) => {
 
     useEffect(() => {
         const handleKeyDown = async (event: KeyboardEvent) => {
-            event.preventDefault();
-            setAnswer([...answer, event.key]);
 
-            // if (isVerifying || isCorrect) return;
-            // if (isInvalidInputChar(event)) return;
+            if (isVerifying || isCorrect) return;
+            if (isInvalidInputChar(event)) return;
 
-            // setIsTyping(true);
+            setIsTyping(true);
 
 
-            // if (event.key === "Enter" && event.code === "Enter") {
-            //     if (answer.length === 0) {
-            //         setIsTyping(false);
-            //         return;
-            //     }
-            //     if (answer[answer.length - 1] === '\u23ce') return;
+            if (event.key === "Enter" && event.code === "Enter") {
+                if (answer.length === 0) {
+                    setIsTyping(false);
+                    return;
+                }
+                if (answer[answer.length - 1] === '\u23ce') return;
 
-            //     setAnswer([...answer, '\u23ce']);
-            //     setIsVerifying(true);
+                setAnswer([...answer, '\u23ce']);
+                setIsVerifying(true);
 
-            //     const verify = async () => {
-            //         return await verifyAnswer(problemId, answer.join(''));
-            //     }
-            //     const result = await verify();
+                const verify = async () => {
+                    return await verifyAnswer(problemId, answer.join(''));
+                }
+                const result = await verify();
 
-            //     if (result) {
-            //         setIsCorrect(true);
-            //         router.refresh();
-            //     } else {
-            //         setIsCorrect(false);
-            //     }
+                if (result) {
+                    setIsCorrect(true);
+                    router.refresh();
+                } else {
+                    setIsCorrect(false);
+                }
 
-            //     setIsVerifying(false);
+                setIsVerifying(false);
 
-            //     return;
-            // }
+                return;
+            }
 
-            // if (event.code === "Backspace") {
-            //     if (answer.length === 1 || answer.length === 0) {
-            //         setIsTyping(false);
-            //     }
-            //     setIsCorrect(null);
-            //     setAnswer(answer.slice(0, answer.length - 1));
-            // } else {
-            //     if (answer[answer.length - 1] === '\u23ce') return;
-            //     if (event.key.charCodeAt(0) >= 32 && event.key.charCodeAt(0) <= 126)
-            //         setAnswer([...answer, event.key]);
-            // }
+            if (event.code === "Backspace") {
+                if (answer.length === 1 || answer.length === 0) {
+                    setIsTyping(false);
+                }
+                setIsCorrect(null);
+                setAnswer(answer.slice(0, answer.length - 1));
+            } else {
+                if (answer[answer.length - 1] === '\u23ce') return;
+                if (event.key.charCodeAt(0) >= 32 && event.key.charCodeAt(0) <= 126)
+                    setAnswer([...answer, event.key]);
+            }
         };
 
         // @ts-ignore
@@ -94,13 +92,7 @@ export const MobileAnswerInput = ({ problemId }: MobileAnswerInputProps) => {
                     <span key={index} className="m-1 flex h-10 w-10 items-center justify-center border-2 border-dashed border-orange-300 focus:outline-none">{char}</span>
                 ))
             }
-            {isTyping && <Indicator isVerifying={isVerifying} isCorrect={isCorrect} />}
-            {!isTyping && "Tap to answer"}
-            {!isTyping && <span className="relative flex h-2 w-2">
-                <span className="animate-ping absolute -top-2 -right-1 inline-flex h-full w-full rounded-full bg-red-700 opacity-75"></span>
-                <span className="relative -top-2 -right-1 inline-flex rounded-full h-2 w-2 bg-red-500"></span>
-            </span>}
-            <Input ref={inputRef} className="opacity-0" tabIndex={-1} value={answer} />
+            <Input ref={inputRef} tabIndex={-1} />
         </div>
     );
 };
